01a53f86879cbb8ed49ea96ca705a034d8e2f65d
michaelcultrera <michael.cultrera@ontariotechu.net>
2024-02-16 16:11:42 -0500

    availablegames.cpp and availablegames.h added, in progress

---
+++ root/project/availablegames.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include "availablegames.h"
+
+//Method to create a game entry into the AvailableGames file
+File createEntry(std::string gameName, std::string sellerUsername, int gamePrice){
+    //TODO: function body
+
+}
+
+//Method to create an “END” game that is placed at the end of the file
+void createEndUser(){
+    //TODO: function body
+
+}
+
+int main(){
+
+    //TODO: main body
+
+    return 0;
+}
---
+++ root/project/availablegames.h
@@ -0,0 +1,22 @@
+#ifndef AVAILABLEGAMES_H
+#define AVAILABLEGAMES_H
+
+#include "file.h"
+#include <string>
+
+//Class meant to represent a collection of all currently available games in the system
+class AvailableGames: public File
+{
+    //Variables associated with available games collection
+    public:
+        std::string gameName;
+        std::string sellerUsername;
+        int gamePrice; 
+
+};
+
+//Method declarations - see cpp file for more info
+File createEntry(std::string gameName, std::string sellerUsername, int gamePrice);
+void createEndUser();
+
+#endif 
