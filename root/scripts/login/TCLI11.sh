386d5748699f5eb4efd787c2759c08ba74c62582
David Dickson <daviddickson@davids-mbp.home>
2024-03-03 17:24:16 -0500

    Scripts login

---
+++ root/scripts/login/TCLI11.sh
@@ -0,0 +1,24 @@
+# Compile 
+g++ ../../project/main.cpp ../../project/user.cpp ../../project/currentusers.cpp ../../project/availablegames.cpp ../../project/dailytransaction.cpp  ../../project/admin.cpp -o ../../project/main
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+
+    # Run the program with input file and save output to a file
+    ../../project/main < "../../test_cases/login/TCLI11-Invalid Two or More Username Test/TCLI11.in.txt" > "../../scripts/logout/outputTCLI11.txt"
+    # Check if program execution was successful
+    if [ $? -eq 0 ]; then
+        echo "Program executed successfully."
+        # Compare output with expected output
+        diff -q "../../scripts/login/outputTCLI11.txt" "../../test_cases/login/TCLI11-Invalid Two or More Username Test/TCLI11.out.txt"
+        if [ $? -eq 0 ]; then
+            echo "Output matches expected output."
+        else
+            echo "Output does not match expected output."
+        fi
+    else
+        echo "Program execution failed."
+    fi
+else
+    echo "Compilation failed. Exiting..."
+fi
