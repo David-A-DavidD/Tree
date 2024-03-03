37e732ca623ac912ef22975d007b4320a9ad91d4
michaelcultrera <michael.cultrera@ontariotechu.net>
2024-03-03 17:33:29 -0500

    update script

--- root/scripts/test_script.sh
+++ root/scripts/test_script.sh
@@ -2,15 +2,22 @@
 
 # This script must be run from Tree/root/scripts directory, where this script resides
 
+cd ../project
+
+g++ main.cpp user.cpp currentusers.cpp availablegames.cpp dailytransaction.cpp admin.cpp -o main
+
 cd ../test_cases 
-for transaction in * ; do
+for transaction in */; do #loop through transactions
     cd "$transaction"
     for testcase in * ; do
         cd "$testcase"
-        # This is where system is run with specific input
-
-        # Save output and place in output directory to be compared with test case
-        
+        # pwd
+        input_file=$(ls | head -n 1)
+        echo "$input_file"
+        # while IFS= read -r line; do
+        #     echo "$line" | ../../../project/user
+        # done < "$input_file" > output.txt
+        ../../../project/main < "$input_file" > output.txt
         cd ..
     done
     cd ..
