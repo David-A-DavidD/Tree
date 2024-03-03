e9d33e77901d1b9497052641405a506ccd433dcd
Ju5t1nLi <justin.li3@ontariotechu.net>
2024-03-03 15:24:22 -0500

    Added refund scripts

---
+++ root/scripts/refund/TCR10.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Compile 
+g++ ../../project/main.cpp ../../project/user.cpp ../../project/currentusers.cpp ../../project/availablegames.cpp ../../project/dailytransaction.cpp  ../../project/admin.cpp -o ../../project/main
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+
+    # Run the program with input file and save output to a file
+    ../../project/main < "../../test_cases/refund/TCR10 - Transaction Code Validity (fail)/TCR10.in.txt" > "../../scripts/refund/outputTCR10.txt"
+
+    # Check if program execution was successful
+    if [ $? -eq 0 ]; then
+        echo "Program executed successfully."
+        # Compare output with expected output
+        diff -q "../../scripts/refund/outputTCR10.txt" "../../test_cases/refund/TCR10 - Transaction Code Validity (fail)/TCR10.out.txt"
+        if [ $? -eq 0 ]; then
+            echo "Output matches expected output."
+        else
+            echo "Output does not match expected output."
+        fi
+    else
+        echo "Program execution failed."
+    fi
+else
+    echo "Compilation failed. Exiting..."
+fi
---
+++ root/scripts/refund/TCR11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Compile 
+g++ ../../project/main.cpp ../../project/user.cpp ../../project/currentusers.cpp ../../project/availablegames.cpp ../../project/dailytransaction.cpp  ../../project/admin.cpp -o ../../project/main
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+
+    # Run the program with input file and save output to a file
+    ../../project/main < "../../test_cases/refund/TCR11 - Username Validity (Buyer) (success)/TCR11.in.txt" > "../../scripts/refund/outputTCR11.txt"
+
+    # Check if program execution was successful
+    if [ $? -eq 0 ]; then
+        echo "Program executed successfully."
+        # Compare output with expected output
+        diff -q "../../scripts/refund/outputTCR11.txt" "../../test_cases/refund/TCR11 - Username Validity (Buyer) (success)/TCR11.out.txt"
+        if [ $? -eq 0 ]; then
+            echo "Output matches expected output."
+        else
+            echo "Output does not match expected output."
+        fi
+    else
+        echo "Program execution failed."
+    fi
+else
+    echo "Compilation failed. Exiting..."
+fi
