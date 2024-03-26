7aa0aaebe6a685bebdfa48b4010d36bdb08a92a2
Ju5t1nLi <justin.li3@ontariotechu.net>
2024-03-26 14:13:45 -0400

    updates

--- root/project/currentusers_pytests/test_decreaseBalance.py
+++ root/project/currentusers_pytests/test_decreaseBalance.py
@@ -2,12 +2,21 @@ from currentusers import CurrentUser
 # python3 -m pytest in project folder to run pytests. Must have test_<name of test>.py format to be run as a pytest
 
 def test_decreaseBalance9S(capfd):
+    filepath = "currentusers.txt"
+
+    with open(filepath, 'r') as file:
+        original_content = file.read()
+ 
+    with open(filepath, 'w') as file:
+        file.write("Test_SS_________SS_500.00000")
+
     CurrentUser.decreaseBalance("Test SS", 500)
     out, err = capfd.readouterr()
 
     assert out == "Balance for Test SS decreased to 0.0.\n"
 
-    CurrentUser.increaseBalance("Test SS", 500)
+    with open(filepath, 'w') as file:
+        file.write(original_content)
 
 def test_decreaseBalance9F(capfd):
     CurrentUser.decreaseBalance("Aidan", 10)
