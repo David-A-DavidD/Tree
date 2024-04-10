073fea11c96bc14c898605f7e12e8cef9a18e9b0
michaelcultrera <michael.cultrera@ontariotechu.net>
2024-04-10 16:43:07 -0400

    Fixed mergeDTF method

--- root/project/dailytransaction.py
+++ root/project/dailytransaction.py
@@ -13,17 +13,17 @@ class DailyTransaction:
             print("DTF file successfully cleared.")
 
 
-    # # Merge all daily transaction files produced for that day
-    # def mergeDTF():
-    #     with open("./dailytransaction_files/dailytransactions_merged.txt", 'w') as out_file:
-    #         for file in os.listdir("./dailytransaction_files"):
-    #             if file.endswith('.txt') and file != "dailytransactions_merged.txt":
-    #                 file_path = os.path.join("./dailytransaction_files", file)
-    #                 with open(file_path, 'r') as in_file:
-    #                     # Read the contents of the dtf file and write to merged dtf file
-    #                     out_file.write(in_file.read())
-    #                     out_file.write('\n')  # Add a newline after each file's content
-    #                     print("DTF file added to dailytransactions_merged.txt.")
+    # Merge all daily transaction files produced for that day
+    def mergeDTF():
+        with open("../testing/scripts/inputs/dailytransactions_merged.txt", 'w') as out_file:
+            for file in os.listdir("../testing/scripts/inputs"):
+                if file[:3] == "dtf":
+                    file_path = os.path.join("../testing/scripts/inputs", file)
+                    with open(file_path, 'r') as in_file:
+                        # Read the contents of the dtf file and write to merged dtf file
+                        out_file.write(in_file.read())
+                        out_file.write('\n')  # Add a newline after each file's content
+                        print("DTF file added to dailytransactions_merged.txt.")
 
 
 
